cd18553fbac0a5403c1673b21839d5e594e11a09
Sebastian <lauraantezanasebastian44@gmail.com>
2022-07-27 10:02:11 -0500

    Parte 2 - Tabla Estudiante - OK

--- CGL_UC4/miapp/models.py
+++ CGL_UC4/miapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    codigo = models.CharField(max_length=10)
+    DNI = models.CharField(max_length=8)
+    nombre = models.CharField(max_length=100)
+    apepat = models.CharField(max_length=80)
+    apemat = models.CharField(max_length=80)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=9)
+    telefono = models.CharField(max_length=9)
